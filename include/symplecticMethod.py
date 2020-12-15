ea16f80ef91c02d124a153430e7c87992176ec92
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2020-12-15 15:52:32 +0000

    Fixed error pertaining to missing exponents in the potential evolution.

--- include/symplecticMethod.py
+++ include/symplecticMethod.py
@@ -6,11 +6,9 @@ import cupy as cp
 def kinetic_evolution(wfn_k, time_step, wvn_x, wvn_y):
     wfn_k *= cp.exp(-0.25 * 1j * time_step * (wvn_x ** 2 + wvn_y ** 2))
 
-    return wfn_k
-
 
 def potential_evolution(wfn_1_old, wfn_2_old, time_step, inter_int, intra_int):
-    wfn_1 = wfn_1_old * cp.exp(-1j * time_step * (intra_int * cp.abs(wfn_1_old) + inter_int * cp.abs(wfn_2_old)))
-    wfn_2 = wfn_2_old * cp.exp(-1j * time_step * (intra_int * cp.abs(wfn_2_old) + inter_int * cp.abs(wfn_1_old)))
+    wfn_1 = wfn_1_old * cp.exp(-1j * time_step * (intra_int * cp.abs(wfn_1_old) ** 2 + inter_int * cp.abs(wfn_2_old) ** 2))
+    wfn_2 = wfn_2_old * cp.exp(-1j * time_step * (intra_int * cp.abs(wfn_2_old) ** 2 + inter_int * cp.abs(wfn_1_old) ** 2))
 
     return wfn_1, wfn_2
