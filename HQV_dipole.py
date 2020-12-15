1b4413dd415d0cde3e27ad6d010ebceed3210d5d
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2020-12-15 19:06:57 +0000

    Added real time evolution.

--- HQV_dipole.py
+++ HQV_dipole.py
@@ -3,7 +3,6 @@ import cupy as cp
 import h5py
 from include.phaseImprinting import get_phase
 import include.symplecticMethod as sm
-import matplotlib.pyplot as plt
 
 """Creates a HQV dipole in one of the components of the system and evolves the dynamics in real time."""
 
@@ -39,10 +38,6 @@ Nframe = 1000   # Save data every Nframe number of timesteps
 dt = 1e-2  # Imaginary time timestep
 t = 0.
 save_index = 0   # Array index
-
-filename = 'HQV_dipole'    # Name of file to save data to
-data_path = '../scratch/data/scalar/{}.hdf5'.format(filename)
-backup_data_path = '../scratch/data/scalar/{}_backup.hdf5'.format(filename)
 
 # --------------------------------------------------------------------------------------------------------------------
 # Generate the initial state:
@@ -103,3 +98,65 @@ for i in range(2000):
     psi_2 *= cp.exp(1j * theta_fix_2) / cp.exp(1j * cp.angle(psi_2))
     psi_1_k = cp.fft.fft2(psi_1)
     psi_2_k = cp.fft.fft2(psi_2)
+
+# ------------------------------------------------------------------------------------------------------------------
+# Creating save file and saving initial data
+# ------------------------------------------------------------------------------------------------------------------
+filename = 'HQV_dipole'    # Name of file to save data to
+data_path = 'data/{}.hdf5'.format(filename)
+
+with h5py.File(data_path, 'w') as data:
+    # Saving spatial data:
+    data.create_dataset('grid/x', x.shape, data=cp.asnumpy(x))
+    data.create_dataset('grid/y', y.shape, data=cp.asnumpy(y))
+
+    # Saving time variables:
+    data.create_dataset('time/Nt', data=Nt)
+    data.create_dataset('time/dt', data=dt)
+    data.create_dataset('time/Nframe', data=Nframe)
+
+    # Creating empty wavefunction datasets to store data:
+    data.create_dataset('wavefunction/psi_1', (Nx, Ny, 1), maxshape=(Nx, Ny, None), dtype='complex64')
+    data.create_dataset('wavefunction/psi_2', (Nx, Ny, 1), maxshape=(Nx, Ny, None), dtype='complex64')
+
+    # Stores initial state:
+    data.create_dataset('initial_state/psi_1', data=cp.asnumpy(cp.fft.ifft2(psi_1_k)))
+    data.create_dataset('initial_state/psi_2', data=cp.asnumpy(cp.fft.ifft2(psi_2_k)))
+
+# ------------------------------------------------------------------------------------------------------------------
+# Real time evolution
+# ------------------------------------------------------------------------------------------------------------------
+for i in range(Nt):
+    # Kinetic step:
+    sm.kinetic_evolution(psi_1_k, dt, Kx, Ky)
+    sm.kinetic_evolution(psi_2_k, dt, Kx, Ky)
+
+    psi_1 = cp.fft.ifft2(psi_1_k)
+    psi_2 = cp.fft.ifft2(psi_2_k)
+
+    # Potential step:
+    psi_1, psi_2 = sm.potential_evolution(psi_1, psi_2, dt, g1, g2, g12)
+
+    psi_1_k = cp.fft.fft2(psi_1)
+    psi_2_k = cp.fft.fft2(psi_2)
+
+    # Kinetic step:
+    sm.kinetic_evolution(psi_1_k, dt, Kx, Ky)
+    sm.kinetic_evolution(psi_2_k, dt, Kx, Ky)
+
+    # Saves data
+    if np.mod(i + 1, Nframe) == 0:
+        with h5py.File(data_path, 'r+') as data:
+            new_psi_1 = data['wavefunction/psi_1']
+            new_psi_2 = data['wavefunction/psi_2']
+            new_psi_1.resize((Nx, Ny, save_index + 1))
+            new_psi_2.resize((Nx, Ny, save_index + 1))
+            new_psi_1[:, :, save_index] = cp.asnumpy(cp.fft.ifft2(psi_1_k))
+            new_psi_2[:, :, save_index] = cp.asnumpy(cp.fft.ifft2(psi_2_k))
+        save_index += 1
+
+    # Prints current time
+    if np.mod(i, Nframe) == 0:
+        print('t = %1.4f' % t)
+
+    t += dt
