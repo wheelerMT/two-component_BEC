0cca1ac1eec0ff00d805b2607eb8c16928af6db0
Matt Wheeler <matthew.t.wheeler@uea.ac.uk>
2021-02-05 11:18:32 +0000

    Changed figure size. Updated cvals for both density and pseudo-vort plots. Fixed erroneous plotting of the psi_1 component on the psi_2 plot.

--- plots/animations.py
+++ plots/animations.py
@@ -40,7 +40,7 @@ num_of_frames = psi_1.shape[-1]
 # ------------------------------------------------------------------------------------------------------------------
 # Set up initial figure
 # ------------------------------------------------------------------------------------------------------------------
-fig, ax = plt.subplots(2, 2, figsize=(12, 10), dpi=150)
+fig, ax = plt.subplots(2, 2, figsize=(6.4*2, 9.6), dpi=150)
 ax[0, 0].set_ylabel(r'$y / a_s$')
 ax[1, 0].set_ylabel(r'$y / a_s$')
 ax[0, 0].set_title(r'$|\psi_1|^2$')
@@ -51,8 +51,8 @@ for axis in ax[1, :]:
     axis.set_xlabel(r'$x / a_s$')
 
 # Initial frame plot:
-densPlus_plot = ax[0, 0].pcolormesh(X, Y, abs(psi_1[:, :, 0]) ** 2, vmin=0, vmax=3500, shading='auto', cmap='gnuplot')
-densMinus_plot = ax[0, 1].pcolormesh(X, Y, abs(psi_2[:, :, 0]) ** 2, vmin=0, vmax=3500, shading='auto', cmap='gnuplot')
+densPlus_plot = ax[0, 0].pcolormesh(X, Y, abs(psi_1[:, :, 0]) ** 2, vmin=0, vmax=4500, shading='auto', cmap='gnuplot')
+densMinus_plot = ax[0, 1].pcolormesh(X, Y, abs(psi_2[:, :, 0]) ** 2, vmin=0, vmax=4500, shading='auto', cmap='gnuplot')
 
 # Set up color bar:
 dens_cbar = plt.colorbar(densMinus_plot, ax=ax[0, 1], fraction=0.044, pad=0.03)
@@ -66,8 +66,8 @@ dpsi_y_2 = spectral_derivative(psi_2[:, :, 0], Ky)
 pseudo_vort_1 = 2 * (np.conj(dpsi_x_1) * dpsi_y_1 - dpsi_x_1 * np.conj(dpsi_y_1)) / (2 * 1j)
 pseudo_vort_2 = 2 * (np.conj(dpsi_x_2) * dpsi_y_2 - dpsi_x_2 * np.conj(dpsi_y_2)) / (2 * 1j)
 
-pseudoVortPlus_plot = ax[1, 0].pcolormesh(X, Y, pseudo_vort_1.real, vmin=-500, vmax=500, shading='auto', cmap='jet')
-pseudoVortMinus_plot = ax[1, 1].pcolormesh(X, Y, pseudo_vort_2.real, vmin=-500, vmax=500, shading='auto', cmap='jet')
+pseudoVortPlus_plot = ax[1, 0].pcolormesh(X, Y, pseudo_vort_1.real, vmin=-300, vmax=300, shading='auto', cmap='jet')
+pseudoVortMinus_plot = ax[1, 1].pcolormesh(X, Y, pseudo_vort_2.real, vmin=-300, vmax=300, shading='auto', cmap='jet')
 
 vort_cbar = plt.colorbar(pseudoVortMinus_plot, ax=ax[1, 1], fraction=0.044, pad=0.03)
 plt.tight_layout()
@@ -88,7 +88,7 @@ def animate(i):
     pseudo_vort_2 = (2 * (np.conj(dpsi_x_2) * dpsi_y_2 - dpsi_x_2 * np.conj(dpsi_y_2)) / (2 * 1j)).real
 
     densPlus_plot.set_array((abs(psi_1[:, :, i]) ** 2).ravel())
-    densMinus_plot.set_array((abs(psi_1[:, :, i]) ** 2).ravel())
+    densMinus_plot.set_array((abs(psi_2[:, :, i]) ** 2).ravel())
 
     pseudoVortPlus_plot.set_array(pseudo_vort_1.ravel())
     pseudoVortMinus_plot.set_array(pseudo_vort_2.ravel())
